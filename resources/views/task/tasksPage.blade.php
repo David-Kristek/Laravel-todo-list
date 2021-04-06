2b74d9682e3256ff05c83741eeacfd64da463252
David Křístek <david.kristek05@gmail.com>
2021-04-06 22:06:54 +0200

    single

--- resources/views/task/tasksPage.blade.php
+++ resources/views/task/tasksPage.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mt-5">
-        <h1 class="text text-center badge-secondary pt-2 pb-2 m-0">Tasks</h1>
+        <h1 class="text text-center badge-secondary pt-2 pb-2 m-0">Tasksa</h1>
         <div class="container p-5" style="background-color:rgb(240, 240, 240)">
             @forelse ($tasks as $task)
                 <div class="border badge-light rounded h5 d-flex flex-row justify-content-between pl-4 pr-4 align-items-center"
