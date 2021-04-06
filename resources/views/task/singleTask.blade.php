df4c54ff5031676c648e5bb72b2770bc8b19061c
David Křístek <david.kristek05@gmail.com>
2021-04-06 22:22:37 +0200

    single Task

--- resources/views/task/singleTask.blade.php
+++ resources/views/task/singleTask.blade.php
@@ -1,19 +1,15 @@
 @extends('layout.app')
 
 @section('content')
-<div class="container mt-5">
-    <h1 class="text text-center badge-secondary pt-2 pb-2 m-0">Tasks</h1>
-    <div class="container p-5" style="background-color:rgb(240, 240, 240)">
-            <div class="border badge-light rounded h5 d-flex flex-row justify-content-between pl-4 pr-4 align-items-center"
-                style="height:60px" alt="75x75">
-                <p class="p-3 d-inline-block m-0">{{ $task->name }}</p>
-                <div>
-                    <button class="btn btn-primary">View</button>
-                    <a href="{{ route("taskCompleted", $task) }}" class="btn btn-success">Completed</a>
-                </div>
-
+    <div class="container mt-5">
+        <h1 class="text text-center badge-secondary pt-2 pb-2 m-0">Tasks</h1>
+        <div class="container p-4" style="background-color:rgb(240, 240, 240); height:300px">
+            <h2 class="p-3 m-0 text-center badge-light border-bottom">{{ $task->name }}</h2>
+            <p style="font-size:20px; min-height:80px;" class="badge-light p-2 text-center">{{ $task->body }}</p>
+            <div class="float-right">
+                <a href="{{ route("taskView", $task) }}" class="btn btn-primary">Update</a>
+                <a href="{{ route('taskCompleted', $task) }}" class="btn btn-success">Completed</a>
             </div>
-            <p>No tasks, <a href="{{ route('createTask') }}"> add task</a></p>
+        </div>
     </div>
-</div>
 @endsection
